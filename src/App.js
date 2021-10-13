37d4ec55c7b5025e0768043e98d2a79a1eb8bed6
Kesing Huang <kesing.huang@gmail.com>
2021-10-13 11:37:12 +0200

    search function done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import {
+  TextField,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableBody,
+  Paper,
+  Box,
+} from "@mui/material";
+import axios from "axios";
+// import { application } from "express";
 
 function App() {
+  const [input, setInput] = React.useState("");
+  const [shows, setShows] = React.useState();
+  const fetchData = async (input) => {
+    const res = await axios.get(
+      `https://api.tvmaze.com/search/shows?q=${input}`
+    );
+    console.log("res", res);
+    if (res.data.length > 0) {
+      const shows = res.data.map((data) => data.show);
+      console.log("shows", shows);
+      setShows(shows);
+    }
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    console.log("event: ", e.target.value);
+  };
+  React.useEffect(() => {
+    fetchData(input);
+    console.log("input:", input);
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          p: 1,
+          m: 1,
+          bgcolor: "background.paper",
+        }}
+      >
+        <TextField
+          id="search-input"
+          label="Search Tv show"
+          variant="standard"
+          placeholder="Search"
+          value={input}
+          onChange={handleChange}
+        />
+      </Box>
+      {shows ? (
+        <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          p: 1,
+          m: 1,
+          bgcolor: "background.paper",
+        }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="showsTable">
+              <TableHead>
+                <TableRow>
+                  <TableCell> Name</TableCell>
+                  <TableCell> Runtime</TableCell>
+                  <TableCell> Rating</TableCell>
+                  <TableCell> Premiered</TableCell>
+                  <TableCell> Link</TableCell>
+
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {shows.map((show) => (
+                  <TableRow
+                    key={show.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell>{show.name}</TableCell>
+                    <TableCell>{show.runtime}</TableCell>
+                    <TableCell>{show.rating.average}</TableCell>
+                    <TableCell>{show.premiered}</TableCell>
+                    <TableCell>
+                      <a href={show.url}>{show.url}</a>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      ) : null}
+    </>
   );
 }
 
